470882670f69a1713cd3b3a2b997e420a20edcc5
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2021-04-08 19:43:40 -0400

    Added rotating coin

--- ARhunt/ViewController.swift
+++ ARhunt/ViewController.swift
@@ -11,33 +11,55 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
-    
+   
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        
-//        let boxAnchor = try! Experience.loadBox()
+        let boxAnchor = try! Experience.loadBox()
+
+       
+//         Load the "Box" scene from the "Experience" Reality File
+//
 //
 //        // Add the box anchor to the scene
-//        arView.scene.anchors.append(boxAnchor)
-        let anchor = AnchorEntity(plane: .horizontal, classification: .floor, minimumBounds: [0.2,0.2])
-        var cards: [Entity] = []
-        
-        for _ in 1...4{
-            let box = MeshResource.generateBox(width: 0.25, height: 2.5, depth: 0.25)
-            let metalMaterial = SimpleMaterial(color: .cyan, isMetallic: true)
-            let model = ModelEntity(mesh: box, materials: [metalMaterial])
-            cards.append(model)
-        }//for
-        for (index, card) in cards.enumerated(){
-            let x = Float(index % 2)
-            let z = Float(index / 2)
+        arView.scene.anchors.append(boxAnchor)
+            boxAnchor.children[0].transform = Transform(pitch: 0,
+                                                        yaw: 0,
+                                                       roll: .pi/4)
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { timer in
+            let curOrientationAngle = boxAnchor.coin?.orientation.angle
             
-            card.position = [x*1, 0, z*1]
-            anchor.addChild(card)
-        }
-        arView.scene.addAnchor(anchor)
+
+            boxAnchor.coin?.orientation = simd_quatf(angle: (curOrientationAngle! + .pi/100).truncatingRemainder(dividingBy: (2 * 3.14159265)),     /* 45 Degrees */
+                                                          axis: [1, 0, 0])
+            print(boxAnchor.coin?.orientation.vector);
+        })
+      
+       // timer.fire()
+
+
 
     }//viewDidLoad
+    
+    
+   
+
+    
+    //        let anchor = AnchorEntity(plane: .horizontal, classification: .floor, minimumBounds: [0.2,0.2])
+    //        var cards: [Entity] = []
+    //
+    //        for _ in 1...4{
+    //            let box = MeshResource.generateBox(width: 0.25, height: 2.5, depth: 0.25)
+    //            let metalMaterial = SimpleMaterial(color: .cyan, isMetallic: true)
+    //            let model = ModelEntity(mesh: box, materials: [metalMaterial])
+    //            cards.append(model)
+    //        }//for
+    //        for (index, card) in cards.enumerated(){
+    //            let x = Float(index % 2)
+    //            let z = Float(index / 2)
+    //
+    //            card.position = [x*1, 0, z*1]
+    //            anchor.addChild(card)
+    //        }
+    //        arView.scene.addAnchor(anchor)
 }
